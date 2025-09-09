eb81dc90ba119990f89dacce456090ded95388a7
Rayane <raay.rs4@gmail.com>
2025-09-09 17:22:07 -0300

    adiciocando recursividade

--- xadrez.c
+++ xadrez.c
@@ -1,46 +1,76 @@
 #include <stdio.h>
 
+// Torre: Move-se horizontalmente (direita)
+void moverTorre(int casas)
+{
+  if (casas <= 0)
+    return;
+  printf("Direita\n");
+  moverTorre(casas - 1);
+}
+
+// Rainha: Move-se horizontalmente (esquerda)
+void moverRainha(int casas)
+{
+  if (casas <= 0)
+    return;
+  printf("Esquerda\n");
+  moverRainha(casas - 1);
+}
+
+// Bispo: recursivo com loops aninhados (vertical e horizontal)
+void moverBispo(int casasVertical, int casasHorizontal)
+{
+  if (casasVertical <= 0)
+    return;
+  for (int i = 1; i <= casasHorizontal; i++)
+  {
+    printf("Cima, Direita\n");
+  }
+  moverBispo(casasVertical - 1, casasHorizontal);
+}
+
+void moverCavalo()
+{
+  int casasVerticais = 2;
+  int casasHorizontais = 1;
+
+  for (int i = 0; i < casasVerticais; i++)
+  {
+    printf("Cima\n");
+    int j = 0;
+    while (j < casasHorizontais)
+    {
+      if (i == 1 && j == 0)
+      {
+        printf("Direita\n");
+        j++;
+        break;
+      }
+      j++;
+      continue;
+    }
+  }
+}
+
 int main()
 {
-  // Definição das casas a mover para cada peça
   int casasTorre = 5;
   int casasBispo = 5;
+  int casasBispoHorizontal = 1;
   int casasRainha = 8;
 
   printf("Movimento da Torre:\n");
-  for (int i = 1; i <= casasTorre; i++)
-  {
-    printf("Direita\n");
-  }
+  moverTorre(casasTorre);
 
   printf("\nMovimento do Bispo:\n");
-  int j = 1;
-
-  while (j <= casasBispo)
-  {
-    printf("Cima, Direita\n");
-    j++;
-  }
+  moverBispo(casasBispo, casasBispoHorizontal);
 
   printf("\nMovimento da Rainha:\n");
-  int k = 1;
-  do
-  {
-    printf("Esquerda\n");
-    k++;
-  } while (k <= casasRainha);
+  moverRainha(casasRainha);
 
   printf("\nMovimento do Cavalo:\n");
-  for (int m = 1; m <= 2; m++)
-  {
-    printf("Baixo\n");
-  }
-  int n = 1;
-  while (n <= 1)
-  {
-    printf("Esquerda\n");
-    n++;
-  }
+  moverCavalo();
 
   return 0;
 }
